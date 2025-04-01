da8bb9ade773fdb98eac16cbf69413a118c91c9a
hebertmfrancato <hebertmfrancato@gmail.com>
2025-03-31 23:49:49 -0300

    Adiciona teste de Campos obrigatórios

--- web/cypress/e2e/pre-cadastro.cy.js
+++ web/cypress/e2e/pre-cadastro.cy.js
@@ -7,15 +7,20 @@ describe('Pré-Cadastro', () => {
   it('Realizar o pré-cadastro do cliente', () => {
     cy.get('header nav a[href="pre-cadastro"]')
       .click()
+
     cy.get('form h2')
       .should('be.visible')
       .and('have.text', 'Seus dados')
+
     cy.get('input[name="full-name"]')
       .type('Hebert Francato')
+
     cy.get('input[name="email"]')
       .type('hebertfrancato@teste.com')
+
     cy.contains('button[type="submit"]', 'Continuar')
       .click()
+
     cy.get('.user-name')
       .should('be.visible')
       .and('have.text', 'Olá, Hebert')
@@ -23,4 +28,28 @@ describe('Pré-Cadastro', () => {
       .should('be.visible')
       .and('have.text', 'hebertfrancato@teste.com')
   })
+
+  it.only('Campos obrigatórios', () => {
+    cy.get('header nav a[href="pre-cadastro"]')
+      .click()
+
+    cy.get('form h2')
+      .should('be.visible')
+      .and('have.text', 'Seus dados')
+
+    cy.contains('button[type="submit"]', 'Continuar')
+      .click()
+
+    cy.contains('label', 'Nome Completo')
+      .parent()
+      .find('.alert-msg')
+      .should('be.visible')
+      .and('include.text', 'O campo nome é obrigatório.')
+      
+      cy.contains('label', 'E-mail')
+      .parent()
+      .find('.alert-msg')
+      .should('be.visible')
+      .and('include.text', 'O campo e-mail é obrigatório.') 
+  });
 })
