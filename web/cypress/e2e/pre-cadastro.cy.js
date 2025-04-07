252ee937c4d7a69b4c71596adfb77c5d14db6c94
Hebert Francato <hebertfrancato@MacBook-Air-de-Hebert.local>
2025-04-06 21:28:50 -0300

    Adiciona novos cenários e altera imagem docker

--- web/cypress/e2e/pre-cadastro.cy.js
+++ web/cypress/e2e/pre-cadastro.cy.js
@@ -29,7 +29,7 @@ describe('Pré-Cadastro', () => {
       .and('have.text', 'hebertfrancato@teste.com')
   })
 
-  it.only('Campos obrigatórios', () => {
+  it('Campos obrigatórios', () => {
     cy.get('header nav a[href="pre-cadastro"]')
       .click()
 
@@ -51,5 +51,53 @@ describe('Pré-Cadastro', () => {
       .find('.alert-msg')
       .should('be.visible')
       .and('include.text', 'O campo e-mail é obrigatório.') 
+  });
+
+  it('Não deve fazer o pré-cadastro apenas com o primeiro nome', () => {
+    cy.get('header nav a[href="pre-cadastro"]')
+      .click()
+
+    cy.get('form h2')
+      .should('be.visible')
+      .and('have.text', 'Seus dados')
+
+    cy.get('input[name="full-name"]')
+      .type('Hebert')
+
+    cy.get('input[name="email"]')
+      .type('hebertfrancato@teste.com')
+
+    cy.contains('button[type="submit"]', 'Continuar')
+      .click()
+
+    cy.contains('label', 'Nome Completo')
+      .parent()
+      .find('.alert-msg')
+      .should('be.visible')
+      .and('have.text', 'Informe seu nome completo.')
+  });
+
+  it('Não deve fazer o pré-cadastro com o e-mail incorreto', () => {
+    cy.get('header nav a[href="pre-cadastro"]')
+      .click()
+
+    cy.get('form h2')
+      .should('be.visible')
+      .and('have.text', 'Seus dados')
+
+    cy.get('input[name="full-name"]')
+      .type('Hebert Francato')
+
+    cy.get('input[name="email"]')
+      .type('www.teste.com.br')
+
+    cy.contains('button[type="submit"]', 'Continuar')
+      .click()
+
+    cy.contains('label', 'E-mail')
+      .parent()
+      .find('.alert-msg')
+      .should('be.visible')
+      .and('have.text', 'O e-mail inserido é inválido.')
   });
 })
